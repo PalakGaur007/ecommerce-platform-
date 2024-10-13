6bb00b267255eb443e57e81e60283311a76e14bb
PalakGaur007 <145285759+PalakGaur007@users.noreply.github.com>
2024-10-13 17:44:07 +0530

    Create OrderService.java

---
+++ Order Management Service./OrderService.java
@@ -0,0 +1,18 @@
+import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.stereotype.Service;  
+  
+import java.util.List;  
+  
+@Service  
+public class OrderService {  
+    @Autowired  
+    private OrderRepository orderRepository;  
+  
+    public Order saveOrder(Order order) {  
+        return orderRepository.save(order);  
+    }  
+  
+    public List<Order> getAllOrders() {  
+        return orderRepository.findAll();  
+    }  
+}  
